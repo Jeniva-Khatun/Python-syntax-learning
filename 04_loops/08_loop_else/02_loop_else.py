d5426484b3df798a17c4e31fc0125578518d68a1
Jeniva Khatun <khatunjeniva11@gmail.com>
2026-09-04 23:49:55 +0530

    Search for a number using for else

---
+++ 04_loops/08_loop_else/02_loop_else.py
@@ -0,0 +1,13 @@
+# 2. Check whether a number is prime using for...else
+
+n = int(input("Enter a number: "))
+
+if n < 2:
+    print("Not Prime")
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime")
