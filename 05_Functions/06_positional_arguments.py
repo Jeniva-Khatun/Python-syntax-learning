686e2b6c03000f85fd89b61254653d5cfab5e893
Jeniva Khatun <khatunjeniva11@gmail.com>
2026-09-18 13:38:03 +0530

    feat : positional arguments

---
+++ 05_Functions/06_positional_arguments.py
@@ -0,0 +1,10 @@
+#positional arguments
+
+def students(name,roll,batch):
+    print("name : ",name)
+    print("roll : ",roll)
+    print("batch: ",batch)
+
+
+#values are assigned according to the position
+students("Jeniva",150,2)
