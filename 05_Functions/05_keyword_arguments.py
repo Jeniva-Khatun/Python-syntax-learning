5c58d6b7eaa61f06beaae643f2cdf0a0e63f514a
Jeniva Khatun <khatunjeniva11@gmail.com>
2026-09-18 13:37:14 +0530

    feat :  Keyword arguments

---
+++ 05_Functions/05_keyword_arguments.py
@@ -0,0 +1,13 @@
+# Keyword arguments
+
+def student(name, age, course):
+    print("Name:", name)
+    print("Age:", age)
+    print("Course:", course)
+
+
+student(
+    course="CSE",
+    name="Jeniva",
+    age=20
+)
